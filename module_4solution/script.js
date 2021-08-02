fd9201142ac6b788fbccfc39859fc0e00e1749d8
Yasharth-gupta <yash02082000@gmail.com>
2021-08-02 10:48:40 +0530

    Update script.js

--- module_4solution/script.js
+++ module_4solution/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Harry";
-names[1]="Jen";
-names[2]="abshiek";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
 names[3]="jason";
-names[4]="pause";
-names[5]="jimy";
-names[6]="John";
-names[7]="Yamuna";
-names[8]="lara";
-names[9]="Jonny";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
